95dc13840471225fed739804524199c04e16edc1
ki11e6 <sharath.surendran.m@gmail.com>
2024-12-27 00:17:52 +0530

    updated some notes

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -3,14 +3,23 @@ struct Deck {
     cards: Vec<String>,
 }
 
+//this is inherent implientation with impl
+// impl is a keyword used to create an implementation block that add function to the struct.
+//they have same name as struct that's why its called inherent implementation.
+// impl is used to create methods and functions that are associated with the struct.
 impl Deck {
+    // return type inotation with return type Deck or Self as function is inside Deck and returning it to Dect parent.
+    //this is associated function
     fn new() -> Self {
+        //following are area the has fixed values
         let suits = ["hearts", "spades", "clubs", "diamonds"];
         let values = [
             "ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack",
             "queen", "king",
         ];
 
+        //below is vector arrays that has dinamic array
+        // let mut cards = vec::new(); /*you can also use this */
         let mut cards = vec![]; // add mute to make the binding mutable.
 
         for suit in suits {
@@ -20,8 +29,7 @@ impl Deck {
             }
         }
 
-        let dec = Deck { cards: cards };
-        return dec;
+        return Deck { cards };
     }
 }
 
